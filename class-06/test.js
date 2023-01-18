7ea508ff37d7f23eeaa355f85349ec388e1ed076
faisalahammed044274 <setu2014@gmail.com>
2023-01-18 17:30:43 +0600

    JS

--- class-06/test.js
+++ class-06/test.js
@@ -1,5 +1,10 @@
 function hello() {
-    return document.write("Hello World");
+    return document.write("<h1>Hello World from JS</h1>");
 }
 
 hello();
+function hello1() {
+    return document.write("<h1>JavaScript DOM</h1>");
+}
+
+hello1();
