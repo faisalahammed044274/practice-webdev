a7114094b88fdc3c9d6a76e2c1b032eb673b0e89
faisalahammed044274 <setu2014@gmail.com>
2023-03-26 14:37:49 +0600

    Javascript

---
+++ class-26/javascript/js/scripts.js
@@ -0,0 +1,22 @@
+// alert("I am faisal");
+//document.write('This is Faisal Ahammed');
+// console.log("I am Faisal");
+
+
+
+$myName = '<h4>Okay, I remember that your name is Faisal Ahammed.</h4>';
+
+document.write($myName);
+
+// String = 'hi','122';
+
+// Integer = 1,2,3 
+
+//float = 1.34, 3.54
+
+//array = array(1,3,4,5,hey,hello)
+
+//boolean = true, false
+
+//concate = +
+
