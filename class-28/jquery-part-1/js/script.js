4e176248a1d1c9f54f5f00f42d53f99acecccdd5
faisalahammed044274 <setu2014@gmail.com>
2023-03-27 04:48:13 +0600

    jQuery

--- class-28/jquery-part-1/js/script.js
+++ class-28/jquery-part-1/js/script.js
@@ -1,1 +1,12 @@
-$("p").hide();
+jQuery("p").hide();
+
+var dhori = jQuery('h5').text();
+
+jQuery("h5").html("<h1>"+"dhori (kiiii???)"+"</h1>");
+
+// jQuery("h5").css('color','red');
+// jQuery("h5").css('background','grey');
+// jQuery("h5").css('padding','10px');
+
+
+jQuery("h5").css('color','green').css('background','lightgrey').css('padding','10px').css('text-shadow',('2px 2px #ff0000'));
