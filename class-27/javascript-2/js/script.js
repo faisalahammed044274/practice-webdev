1c1ec7b3043eb8459e0422dee526091adc8c22af
faisalahammed044274 <setu2014@gmail.com>
2023-03-26 23:06:41 +0600

    JavaScript

---
+++ class-27/javascript-2/js/script.js
@@ -0,0 +1,40 @@
+var rohim=20;
+
+var korim=10;
+
+console.log(rohim+korim);
+alert(rohim+korim);
+document.write('Both have '+ (rohim + korim) + ' Taka');
+
+// var amr = {"profession":"Web Developer","age" : 30, "height" :5.6,"skills" : "Hibijibi"};
+
+// alert("Amar Profession : " + amr["profession"]);
+// alert("Amar Age : " + amr["age"]);
+// alert("Amar Height : " + amr["height"]);
+// alert("Amar Skills : " + amr['skills']);
+
+// document.write("Amar Profession : " + amr["profession"]);
+// document.write("Amar Age : " + amr["age"]);
+// document.write("Amar Height : " + amr["height"]);
+// document.write("Amar Skills : " + amr['skills']);
+
+
+var content = "We are Learning Javascript, we are Learning PHP indeed";
+document.write('<br>')
+document.write(content + "</br>");
+document.write(content.length);
+
+var indx = content.indexOf("Javascript");
+document.write('<br>')
+document.write(indx);
+var indx2 = content.lastIndexOf("indeed");
+console.log(indx2);
+alert(indx2);
+
+function ami(){
+    document.write('<br>')
+    alert('Welcome to JavaScript\'s World');
+    document.write('Welcome to JavaScript\'s World');
+}
+
+ami();
