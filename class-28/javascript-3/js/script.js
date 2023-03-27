d1d64116255229f4b66e2d8685306fdb87c60c28
faisalahammed044274 <setu2014@gmail.com>
2023-03-27 18:09:31 +0600

    JavaScript

---
+++ class-28/javascript-3/js/script.js
@@ -0,0 +1,16 @@
+var something = new Object();
+
+something.first = "This is our first line";
+
+something.second = "This is the 2nd line";
+
+something.allData= function(){
+   return this.first + " and " + this.second;
+}
+
+
+alert(something.allData());
+
+function adsClose(){
+    document.getElementById("advertisement").innerHTML="";
+}
