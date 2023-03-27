6ffe48ea4644aeed5baeaa0b16a53efc1a45e694
faisalahammed044274 <setu2014@gmail.com>
2023-03-28 03:46:43 +0600

    jQuery

--- class-28/jquery-part-2/js/script.js
+++ class-28/jquery-part-2/js/script.js
@@ -1,12 +1,21 @@
-jQuery("p").hide();
+// jQuery("p").hide();
 
-var dhori = jQuery('h5').text();
+// var dhori = jQuery('h5').text();
 
-jQuery("h5").html("<h1>"+"dhori (kiiii???)"+"</h1>");
+// jQuery("h5").html("<h1>"+"dhori (kiiii???)"+"</h1>");
 
 // jQuery("h5").css('color','red');
 // jQuery("h5").css('background','grey');
 // jQuery("h5").css('padding','10px');
 
 
-jQuery("h5").css('color','green').css('background','lightgrey').css('padding','10px').css('text-shadow',('2px 2px #ff0000'));
+jQuery("h5").css('color','grey').css('background','yellow').css('padding','10px').css('text-shadow',('2px 2px #ff0000'));
+
+jQuery(".para").addClass("myColor");
+
+jQuery(".para").click(
+    function(){
+        jQuery(".heading3").toggle();
+    }
+);
+
