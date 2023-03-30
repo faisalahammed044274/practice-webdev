f08c421d4e1f2222545edd6d59da52c71edb9cbf
faisalahammed044274 <setu2014@gmail.com>
2023-03-30 06:12:12 +0600

    menu hide show with removeAttr

--- class-29/menu/js/script.js
+++ class-29/menu/js/script.js
@@ -7,11 +7,13 @@ jQuery(document).ready(function(){
     jQuery(window).resize(function(){
         var screenWidth = jQuery(window).width();
         if(screenWidth > 768){
-        jQuery(".main-menu ul").show();
-    }else{
-        jQuery(".main-menu ul").hide();
+        // jQuery(".main-menu ul").show();
+        jQuery(".main-menu ul").removeAttr("style");
+    }
+    // else{
+    //     jQuery(".main-menu ul").hide();
 
-        }
+    //     }
     });
 
 
