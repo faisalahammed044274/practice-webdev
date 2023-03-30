77c7c9cf1ccd5b6f20ac760a4c76747dabe97111
faisalahammed044274 <setu2014@gmail.com>
2023-03-30 06:06:22 +0600

    jQuery Menu Hide Show

---
+++ class-29/menu/js/script.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function(){
+
+    jQuery(".main-menu .bar i").click(function(){
+         jQuery(".main-menu ul").slideToggle();
+    });
+
+    jQuery(window).resize(function(){
+        var screenWidth = jQuery(window).width();
+        if(screenWidth > 768){
+        jQuery(".main-menu ul").show();
+    }else{
+        jQuery(".main-menu ul").hide();
+
+        }
+    });
+
+
+});
