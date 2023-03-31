5819de9383cdba7e585b16535ffe3a298dc1e7b0
faisalahammed044274 <setu2014@gmail.com>
2023-04-01 03:05:50 +0600

    Scrollup btn with fadein fadeout

--- class-29/menu/js/script.js
+++ class-29/menu/js/script.js
@@ -1,45 +1,45 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
+  jQuery(".main-menu .bar i").click(function () {
+    jQuery(".main-menu ul").slideToggle();
+  });
 
-    jQuery(".main-menu .bar i").click(function(){
-         jQuery(".main-menu ul").slideToggle();
-    });
-
-    jQuery(window).resize(function(){
-        var screenWidth = jQuery(window).width();
-        if(screenWidth > 768){
-        // jQuery(".main-menu ul").show();
-        jQuery(".main-menu ul").removeAttr("style");
+  jQuery(window).resize(function () {
+    var screenWidth = jQuery(window).width();
+    if (screenWidth > 768) {
+      // jQuery(".main-menu ul").show();
+      jQuery(".main-menu ul").removeAttr("style");
     }
     // else{
     //     jQuery(".main-menu ul").hide();
 
     //     }
-    });
+  });
 
+  jQuery(".main-menu ul li").hover(function () {
+    jQuery(this).children("ul").slideToggle(300);
+  });
+  // function(){
+  //     jQuery(this).children("ul").hide();
+  // });
 
-    jQuery(".main-menu ul li").hover(
-        function(){
-            jQuery(this).children("ul").slideToggle(300);
-        });
-        // function(){
-        //     jQuery(this).children("ul").hide();
-        // });
+  jQuery(".main-menu ul ul")
+    .parent("li")
+    .children("a")
+    .append('&nbsp;&nbsp;<i class="fas fa-angle-down"></i>');
 
-    jQuery(".main-menu ul ul").parent("li").children("a").append('&nbsp;&nbsp;<i class="fas fa-angle-down"></i>');
+  jQuery(".scrollUpArrow").click(function () {
+    jQuery("html").animate({ scrollTop: "1450px" }, 2000);
+  });
 
+  jQuery(window).scroll(function () {
+    var utd = jQuery(window).scrollTop();
 
-    jQuery(window).scroll(function(){
+    if (utd > 300) {
+      jQuery(".scrollUpArrow").fadeIn();
+    } else {
+      jQuery(".scrollUpArrow").fadeOut();
+    }
+  });
 
-var utd = jQuery(window).scrollTop();
-
-if (utd>200){
-    jQuery('p').show();
-} else{
-jQuery('p').removeAttr('style');
-}
-    });
-
-
-    var headerHeight = jQuery(".header-area").OuterHeight();
-
+  var headerHeight = jQuery(".header-area").OuterHeight();
 });
