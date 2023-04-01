1a7e6be867a93ec84e2bee1f19021342af33af63
faisalahammed044274 <setu2014@gmail.com>
2023-04-02 01:20:45 +0600

    focused & blur

--- class-29/jQuery-4/js/script.js
+++ class-29/jQuery-4/js/script.js
@@ -1,3 +1,29 @@
-jQuery(document).ready(function(){
-    
+jQuery(document).ready(function () {
+  //   var data1 = jQuery(".data1").val();
+  //   var data2 = jQuery(".data2").val();
+
+  //   var bothData = data1 + data2;
+
+  //   jQuery("form").submit(function () {
+  //     jQuery(this).after(bothData);
+  //   });
+
+  // jQuery("form").submit(function () {
+  //   var firstNumber = jQuery(".firstNumber").val();
+  //   var lastNumber = jQuery(".lastNumber").val();
+
+  //   var Addition = parseInt(firstNumber) + parseInt(lastNumber);
+  //   alert(Addition);
+  //   jQuery(this).after(function(){
+  //     document.write("<h3>"+Addition+"</h3>");
+  //   });
+  // });
+
+  jQuery("input[type=text]").focus(function () {
+    jQuery(this).val("this is focused");
+  });
+
+  jQuery("input[type=text]").blur(function () {
+    jQuery(this).val("this is blur");
+  });
 });
