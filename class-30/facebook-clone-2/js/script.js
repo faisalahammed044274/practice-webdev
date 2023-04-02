a4bfd2bf888b4ea5ad7973df60a97ef6c6645594
faisalahammed044274 <setu2014@gmail.com>
2023-04-02 21:33:39 +0600

    Accept added in drag & droppable

--- class-30/facebook-clone-2/js/script.js
+++ class-30/facebook-clone-2/js/script.js
@@ -2,11 +2,12 @@ jQuery(document).ready(function () {
   //   jQuery(".box").draggable({ cursor: "pointer", containment: ".big-box" });
 
   jQuery(".small-box").draggable();
+  jQuery(".red-box").draggable();
 
   jQuery(".big-box").droppable({
     drop: function () {
       jQuery(".overlay").remove();
     },
+    accept: ".red-box",
   });
-  jQuery(".");
 });
