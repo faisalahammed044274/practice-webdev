a2cf74a036a09b06467211ac5f0825ac6d6d65fa
faisalahammed044274 <setu2014@gmail.com>
2023-04-02 19:15:26 +0600

    droppable

--- class-30/js/script.js
+++ class-30/js/script.js
@@ -1,3 +1,13 @@
 jQuery(document).ready(function () {
-  jQuery(".box").draggable({ cursor: "pointer", containment: ".big-box" });
+  //   jQuery(".box").draggable({ cursor: "pointer", containment: ".big-box" });
+
+  jQuery(".box").draggable({
+    cursor: "move",
+  });
+
+  jQuery(".big-box").droppable({
+    drop: function (event, ui) {
+      $(this).addClass("ui-state-highlight").find("h3").html("Dropped!");
+    },
+  });
 });
