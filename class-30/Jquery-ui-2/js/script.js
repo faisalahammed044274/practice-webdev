5ed08087512bc01ea2dd696a41e53aed0ad0791c
faisalahammed044274 <setu2014@gmail.com>
2023-04-03 16:21:08 +0600

    jQuery

---
+++ class-30/Jquery-ui-2/js/script.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function () {
+  //   jQuery(".box").draggable({ cursor: "pointer", containment: ".big-box" });
+
+  jQuery(".box").draggable({
+    cursor: "move",
+  });
+
+  jQuery(".big-box").droppable({
+    drop: function (event, ui) {
+      $(this).addClass("ui-state-highlight").find("h3").html("Dropped!");
+    },
+  });
+});
+
+jQuery(document).ready(function () {
+  jQuery(".box-2").resizable({ animate: true });
+  jQuery(".content").selectable();
+  jQuery("#sortable").sortable();
+
+
+});
