aad25fc55af098e52cb4cce16f96e277fcac57a5
faisalahammed044274 <setu2014@gmail.com>
2023-04-04 03:56:28 +0600

    progress bar

--- class-30/Jquery-ui-2/js/script.js
+++ class-30/Jquery-ui-2/js/script.js
@@ -124,4 +124,35 @@ jQuery(document).ready(function () {
   });
 
   jQuery(".main-menu").menu();
+
+  jQuery(".progress-bar").progressbar({
+    value: 67,
+  });
+
+  jQuery(function () {
+    var progressbar = $("#progressbar"),
+      progressLabel = $(".progress-label");
+
+    progressbar.progressbar({
+      value: false,
+      change: function () {
+        progressLabel.text(progressbar.progressbar("value") + "%");
+      },
+      complete: function () {
+        progressLabel.text("Complete!");
+      },
+    });
+
+    function progress() {
+      var val = progressbar.progressbar("value") || 0;
+
+      progressbar.progressbar("value", val + 2);
+
+      if (val < 99) {
+        setTimeout(progress, 80);
+      }
+    }
+
+    setTimeout(progress, 2000);
+  });
 });
