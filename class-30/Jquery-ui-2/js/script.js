01d117219e7d19740c58994ae68ab057411aa0b2
faisalahammed044274 <setu2014@gmail.com>
2023-04-03 17:08:28 +0600

    jQuery AutoComplete

--- class-30/Jquery-ui-2/js/script.js
+++ class-30/Jquery-ui-2/js/script.js
@@ -1,10 +1,25 @@
 jQuery(document).ready(function () {
-
-
-jQuery(document).ready(function () {
-
   jQuery(".content").accordion();
 
-});
+  var avilableTags = [
+    "async",
+    "bootstrap",
+    "C",
+    "C++",
+    "Dom Manipulation",
+    "ECMA Script",
+    "Flutter",
+    "Figma",
+    "google",
+    "Hypertext Preprocessor(php)",
+    "PHP",
+    "jQuery",
+    "JavaScript",
+    "JSON",
+    "Java",
+  ];
 
+  jQuery("input").autocomplete({
+    source: avilableTags,
+  });
 });
