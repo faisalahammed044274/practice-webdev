022b445e718d60fc65c81d2139dcd2da2aeb1a79
faisalahammed044274 <setu2014@gmail.com>
2023-04-05 03:28:18 +0600

    pricing range

--- class-31/jQuery-ui-3/js/script.js
+++ class-31/jQuery-ui-3/js/script.js
@@ -1,13 +1,30 @@
-jQuery(document).ready(function() {
-  jQuery('.slider').slider({
+jQuery(document).ready(function () {
+  jQuery(".slider").slider({
     min: 0,
     max: 255,
-    slide: function(event, ui) {
-      var redValue = jQuery('.red').slider('value');
-      var greenValue = jQuery('.green').slider('value');
-      var blueValue = jQuery('.blue').slider('value');
-      var rgbValue = 'rgb(' + redValue + ',' + greenValue + ',' + blueValue + ')';
-      jQuery('.box').css('background-color', rgbValue);
-    }
+    slide: function (event, ui) {
+      var redValue = jQuery(".red").slider("value");
+      var greenValue = jQuery(".green").slider("value");
+      var blueValue = jQuery(".blue").slider("value");
+      var rgbValue =
+        "rgb(" + redValue + "," + greenValue + "," + blueValue + ")";
+      jQuery(".box").css("background-color", rgbValue);
+    },
+  });
+
+  jQuery(".range").slider({
+    range: "true",
+    min: 1000,
+    max: 20000,
+    values: [1000, 10000],
+    slide: function (event, faisal) {
+      jQuery(".rangeText").text(
+        "Price range : " +
+          faisal.values[0] +
+          " tk to " +
+          faisal.values[1] +
+          "tk"
+      );
+    },
   });
 });
