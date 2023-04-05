77f9d102f73241d251c0e8694622ea6905caaaf0
faisalahammed044274 <setu2014@gmail.com>
2023-04-05 16:09:01 +0600

    mootools

--- class-31/mootools/js/custom.js
+++ class-31/mootools/js/custom.js
@@ -4,6 +4,6 @@
 
 window.addEvent("domready", function () {
   $$("#nav a").addEvent("click", function () {
-    alert("You clicked me!");
+    alert("You clicked me then execute the event after DOMready!");
   });
 });
