7feb4db2f8e5a254d2dcbd6820947ee4dc47f966
faisalahammed044274 <setu2014@gmail.com>
2023-04-05 20:03:40 +0600

    owl carousel

--- class-32/owl-carousel/swiper/js/script.js
+++ class-32/owl-carousel/swiper/js/script.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
-    animateOut: "slideOutDown",
-    animateIn: "flipInX",
-    items: 5,
-    margin: 30,
-    stagePadding: 30,
-    smartSpeed: 450,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    loop: true,
-    margin: 10,
-    responsiveClass: true,
-    merge: true,
+    'animateOut': "slideOutDown",
+    'animateIn': "flipInX",
+    'items': 5,
+    'margin': 30,
+    'stagePadding': 30,
+    'smartSpeed': 450,
+    'autoplay': true,
+    'autoplayTimeout': 2000,
+    'loop': true,
+    'margin': 10,
+    'responsiveClass': true,
+    'merge': true,
   });
 
   $(".play").on("click", function () {
-    owl.trigger("play.owl.autoplay", [3000]);
+    owl.trigger("play.owl.autoplay", [1000]);
   });
   $(".stop").on("click", function () {
     owl.trigger("stop.owl.autoplay");
