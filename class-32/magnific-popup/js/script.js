57c52ee97e48c53141d89a74e9cecf4dc6263249
faisalahammed044274 <setu2014@gmail.com>
2023-04-08 18:56:53 +0600

    magnific popup gallery

--- class-32/magnific-popup/js/script.js
+++ class-32/magnific-popup/js/script.js
@@ -3,3 +3,13 @@ jQuery(document).ready(function () {
     type: "image",
   });
 });
+
+jQuery(document).ready(function () {
+  jQuery(".parent-container").magnificPopup({
+    delegate: "a", // child items selector, by clicking on it popup will open
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
+});
